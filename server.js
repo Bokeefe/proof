b83be7886f0749614e97fb1fb66d7025ace5b99b
Brendan O'Keefe <c-brendan.okeefe@charter.com>
2019-10-21 10:12:44 -0600

    port var

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+const port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -20,6 +21,6 @@ io.on('connection', function(socket) {
     });
 });
 
-http.listen(3000, function() {
-    console.log('listening on *:3000');
+http.listen(port, function() {
+    console.log(`listening on *:${port}`);
 });
